f4fdd75388ef879242d94f1576378c13cc9c0347
Natalle <natallemoura7@gmail.com>
2022-03-24 14:57:52 +0100

    Organized assets and confirm receiving the data

--- src/scripts/FetchData.js
+++ src/scripts/FetchData.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
 export default function FetchData() {
+  // const [status, setStatus] = useState(0); // 0 = loading; 1 = success; 2 = failure
   const [data, setData] = useState([]);
 
   //properties
@@ -11,8 +12,8 @@ export default function FetchData() {
 
   //save the data
   async function loadData(url, setState) {
-    const response = await fetch(url);
-    const json = await response.json();
+    const response = await fetch(url).catch((error) => console.log(error));
+    const json = await response.json().catch((error) => console.log(error));
 
     setState(json);
   }
