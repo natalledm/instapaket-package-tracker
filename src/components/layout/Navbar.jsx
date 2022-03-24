2a20e726ecda0ffe60ee9e7c59b0064ecf1691b4
Natalle <natallemoura7@gmail.com>
2022-03-24 22:14:52 +0100

    nav logo with link navigation

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
+
 import logo from "../../assets/logo.png";
 import "../../styles/layout/navbar.css";
 
 export default function Navbar() {
   return (
     <nav className="navbar-container">
-      <img src={logo} alt="Kiki's logo" className="navbar-logo" />
+      <Link to="/">
+        <img src={logo} alt="Kiki's logo" className="navbar-logo" />
+      </Link>
     </nav>
   );
 }
