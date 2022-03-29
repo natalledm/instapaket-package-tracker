7890352e553cf580c6ef8f65986c82bff8f5e77a
Natalle <natallemoura7@gmail.com>
2022-03-29 20:21:31 +0200

    Fetch error working and safeguard working

--- src/scripts/fetch.js
+++ src/scripts/fetch.js
@@ -1,8 +1,11 @@
-const apiUrl = "https://my.api.mockaroo.com/insta-orders.json?key=e49e6840";
+const apiUrl = "https://my.api.mockaroo.com/insta-orders.json?key=e49e6840aa";
 
 async function fetchParcels() {
   const response = await fetch(apiUrl);
   const parcels = await response.json();
+  if (!response.ok) {
+    throw new Error("Wrong url!");
+  }
   return parcels;
 }
 
